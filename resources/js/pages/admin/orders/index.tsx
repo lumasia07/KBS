a68915957472e4e4f404c661cd481ac3c3e49a38
FRANCIS KIOKO <57718524+frajosan97@users.noreply.github.com>
2026-03-09 14:05:03 +0300

    Edited

--- resources/js/pages/admin/orders/index.tsx
+++ resources/js/pages/admin/orders/index.tsx
@@ -650,6 +650,7 @@ export default function AdminOrderIndex({ paymentMethods = [] }: Props) {
 }
 
 // ============== Sub-components ==============
+
 // Stat Card Component
 const StatCard = ({ title, value, color, icon: Icon }: any) => {
     const colorMap: any = {
